eae4d0927fdb18e48105f08e4f3768199caf80bb
ModabbirHossainAkanda <modabbirhossain041@gmail.com>
2024-03-13 22:49:21 +0200

    function starts

---
+++ function.js
@@ -0,0 +1,13 @@
+// function declaration
+function startFan(){
+    console.log('Stand up');
+    console.log('walk towards the switch');
+    console.log('Press the switch');
+}
+// call the function
+startFan();
+startFan();
+startFan();
+console.log('jhaal muri khabo');
+console.log('dim vaji khabo');
+startFan();
---
+++ parametar2.js
@@ -0,0 +1,26 @@
+function bringSingara(money){
+    console.log('take disen singara :',money);
+    console.log('ai nen singara');
+}
+var taka = 300;
+// bringSingara(taka);
+
+function add(num1, num2){
+    console.log('going to add:', num1, num2);
+}
+
+// add(130, 430);
+
+function sum(a, b, c, d, e){
+    console.log(a, b, c, d, e);
+    var sum = a+ b + c+ d+ e;
+    console.log(sum);
+}
+// sum(15, 30, 45, 10, 280);
+
+function kabir(money){
+    console.log('amr taka lagbe');
+    console.log('aam gache aam jam gache jaam');
+    console.log('take den: ', money);
+}
+kabir(3000);
