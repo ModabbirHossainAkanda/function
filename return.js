ef347c03de2487248d6bd680980afaa03774a73e
ModabbirHossainAkanda <modabbirhossain041@gmail.com>
2024-03-13 23:25:50 +0200

    final

--- return.js
+++ return.js
@@ -3,8 +3,22 @@ function add(number1, number2){
     var sum = number1 + number2;
     // console.log(sum);
     // return 15;
-    return;
+    return sum;
+    console.log('I need more code');
+    return 15;
+    return 'Hello done';
+    return 'I am hungry';
 }
 // add(45, 15);
-var total = add(80, 20);
-console.log('total', total);
+// var total = add(80, 20);
+// console.log('total', total);
+
+function bringsingara(money){
+    var singaraPrice = 10;
+    var quantity = money / singaraPrice;
+    return quantity;
+}
+
+
+var singaras = bringsingara(150);
+console.log('Eating singaras: ', singaras);
