37e528b1221f1124a1da505befeeb7e3de32659f
ModabbirHossainAkanda <modabbirhossain041@gmail.com>
2024-03-14 01:32:17 +0200

    object final

---
+++ object.js
@@ -0,0 +1,23 @@
+var student = {
+    id: 115, 
+    name: 'Salman Khan', 
+    class: 9, 
+    marks: 98 
+}
+var mobile = {
+    brand: 'Samsung',
+    price: 20000,
+    storage: '64gb',
+    camera: '7mp'
+} 
+
+var myComputer ={
+    brand: 'lenovo',
+    price: 50000,
+    color: 'silver',
+    processor: 'i7'
+}
+console.log(myComputer.price);
+console.log(mobile);
+console.log(student.marks);
+
